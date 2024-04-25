5f6f2ba0648751d2e7ea2eb9fc8cfadefc00bc0a
John Freeman <jfreeman08@gmail.com>
2024-04-24 21:00:52 -0500

    Remove reporting mode

--- src/xrpld/app/main/Main.cpp
+++ src/xrpld/app/main/Main.cpp
@@ -376,7 +376,6 @@ run(int argc, char** argv)
         "quorum",
         po::value<std::size_t>(),
         "Override the minimum validation quorum.")(
-        "reportingReadOnly", "Run in read-only reporting mode")(
         "silent", "No output to the console after startup.")(
         "standalone,a", "Run with no peers.")("verbose,v", "Verbose logging.")
 
@@ -398,9 +397,6 @@ run(int argc, char** argv)
         "net", "Get the initial ledger from the network.")(
         "replay", "Replay a ledger close.")(
         "start", "Start from a fresh Ledger.")(
-        "startReporting",
-        po::value<std::string>(),
-        "Start reporting from a fresh Ledger.")(
         "vacuum", "VACUUM the transaction db.")(
         "valid", "Consider the initial ledger a valid network ledger.");
 
@@ -653,17 +649,6 @@ run(int argc, char** argv)
     if (vm.count("start"))
     {
         config->START_UP = Config::FRESH;
-    }
-
-    if (vm.count("startReporting"))
-    {
-        config->START_UP = Config::FRESH;
-        config->START_LEDGER = vm["startReporting"].as<std::string>();
-    }
-
-    if (vm.count("reportingReadOnly"))
-    {
-        config->setReportingReadOnly(true);
     }
 
     if (vm.count("import"))
