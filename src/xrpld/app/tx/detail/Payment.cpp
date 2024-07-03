812b7dd38627360c8893bea1ca6b399b2d2af2eb
Shawn Xie <35279399+shawnxie999@users.noreply.github.com>
2024-07-03 14:11:50 -0400

    Add missing include (#29)
    * fix missing include
    
    * clang

--- src/xrpld/app/tx/detail/Payment.cpp
+++ src/xrpld/app/tx/detail/Payment.cpp
@@ -20,6 +20,7 @@
 #include <xrpld/app/paths/RippleCalc.h>
 #include <xrpld/app/tx/detail/Payment.h>
 #include <xrpld/core/Config.h>
+#include <xrpld/ledger/View.h>
 #include <xrpl/basics/Log.h>
 #include <xrpl/protocol/Feature.h>
 #include <xrpl/protocol/TxFlags.h>
