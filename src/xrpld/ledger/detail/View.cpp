d81492f271f2a425c4c07e070f2d7b3182ba2a0c
John Freeman <jfreeman08@gmail.com>
2024-11-12 15:21:58 -0600

    progress

--- src/xrpld/ledger/detail/View.cpp
+++ src/xrpld/ledger/detail/View.cpp
@@ -2243,7 +2243,7 @@ assetsToSharesDeposit(
         return assets;
     Number shareTotal = getShareTotal(view, vault);
     auto shares = shareTotal * (assets / assetTotal);
-    STAmount amount{MPTAmount{shares}, MPTIssue{vault->at(sfMPTokenIssuanceID)}};
+    STAmount amount{vault->at(sfMPTokenIssuanceID), shares};
     return amount;
 }
 
